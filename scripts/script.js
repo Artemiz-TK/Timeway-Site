4715a9a04ee42ddf4ea3b7e5c7d1c39110219d92
Artemiz <ethernalbluemckart@torboxda39aw3ex5qo2st6.onion>
2025-07-05 20:06:10 -0300

    refactor(open-close-logic): atualiza a lógica de abrir e fechar o menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,30 @@
-const home = document.querySelector("#home");
-const nav = document.querySelector(".nav");
-const about = document.querySelector("#about");
-const footer = document.querySelector("footer");
 const divContainer = document.querySelector(".container-nav");
 const divAux = document.querySelector(".aux-div");
 const menuBar = document.querySelector("#bar-menu");
 const closeMenu = document.querySelector("#bar-close");
+const divFade = document.querySelector(".fade-div");
 
 function hideMenu() {
     divContainer.classList.remove("show");
     divAux.classList.remove("show");
 }
 
+function hideDarkScreen() {
+    divFade.classList.remove("show");
+}
+
 menuBar.addEventListener('click', () => {
     divContainer.classList.add("show");
     divAux.classList.add("show");
+    divFade.classList.add("show");
+});
+
+divFade.addEventListener('click', () => {
+    hideMenu();
+    hideDarkScreen();
 });
 
 closeMenu.addEventListener('click', () => {
     hideMenu();
-});
-
-home.addEventListener('click', () => {
-    hideMenu();
-});
-
-about.addEventListener('click', () => {
-    hideMenu();
-});
-
-footer.addEventListener('click', () => {
-    hideMenu();
-});
+    hideDarkScreen();
+})
